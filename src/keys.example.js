20e343784c782ba41c72ce4deab1facf4576befa
Barry Blaha <barryblaha@gmail.com>
2018-04-05 17:53:07 -0700

    remove infura and etherscan references

--- src/keys.example.js
+++ src/keys.example.js
@@ -1,8 +1,7 @@
 module.exports = {
-  "etherscanKey": "",
   "privateKey": "",
   "testPrivateKey": "e84f3375c0033f91706d34f1ad834c7692e433095e17784a87e6a1db4560c6a0",
-  "infuraKey": "",
+  "airdropThreshold":1000, // minimum amount of tokens in account in order to receive airdrop
   "airdropTotal": 3000000000000000000000,
   "gasPrice": 2000000000,
   "gasLimit": 100000,
